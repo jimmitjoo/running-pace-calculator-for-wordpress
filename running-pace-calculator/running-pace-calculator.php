d894ea43d73f6df4da2edbad0425d4f87bf85966
Jimmie Johansson <jimmitjoo@gmail.com>
2021-04-14 16:59:53 +0200

    Make it a composer package

--- running-pace-calculator/running-pace-calculator.php
+++ running-pace-calculator/running-pace-calculator.php
@@ -13,12 +13,12 @@
  * @package           Running_Pace_Calculator
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/running-pace-calculator-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Running Pace Calculator
+ * Plugin URI:        https://xn--lpning-wxa.se/
+ * Description:       This is a running pace calculator for WordPress.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            HACKson
+ * Author URI:        https://xn--lpning-wxa.se/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       running-pace-calculator
